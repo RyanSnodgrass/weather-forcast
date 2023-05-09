7618af3c5c726965a79c20e9923d9cfbe4756898
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2023-05-09 15:25:10 -0400

    Create private method to build URI components

--- lib/clients/weather_api_client.rb
+++ lib/clients/weather_api_client.rb
@@ -1,4 +1,19 @@
 require "JSON"
+require "net/http"
 
+# This class will be used to make API calls to weatherapi.com.
 class WeatherApiClient
+  class << self
+    API_ROOT_URL = "http://api.weatherapi.com/v1/"
+
+    private
+
+    def build_uri(api_subject, extra_params = {})
+      uri = URI("#{API_ROOT_URL}/#{api_subject}.json")
+      key_param = { key: ENV["WEATHER_API_KEY"] }
+      full_params = key_param.merge(extra_params)
+      uri.query = URI.encode_www_form(full_params)
+      return uri
+    end
+  end
 end
--- spec/lib/clients/weather_api_client_spec.rb
+++ spec/lib/clients/weather_api_client_spec.rb
@@ -1,6 +1,11 @@
 require "clients/weather_api_client"
 
 RSpec.describe WeatherApiClient do
+  let(:subject) { WeatherApiClient }
+
+  before(:each) do
+    stub_const('ENV', {'WEATHER_API_KEY' => 'asdf'})
+  end
   it "loads the json fixture file" do
     json = File.read("spec/fixtures/fake_forecast_response.json")
     data = JSON.parse(json)
@@ -8,5 +13,14 @@ RSpec.describe WeatherApiClient do
   end
   it "ensures WeatherApiClient is defined" do
     expect(WeatherApiClient).to be_a(Class)
+  end
+
+  describe "#build_uri" do
+    it "builds a uri object for the api call with zip params" do
+      params = { q: "46615" }
+      uri = subject.send("build_uri", "forecast", params)
+      expect(uri.request_uri).to eq("/v1//forecast.json?key=asdf&q=46615")
+      expect(uri.origin).to eq("http://api.weatherapi.com")
+    end
   end
 end
