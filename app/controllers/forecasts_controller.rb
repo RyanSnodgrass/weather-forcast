43aea08b1b6ebf23551b0ae4541266ff3ab28c65
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2023-05-11 16:35:16 -0400

    Make default your browser location with Geolocator (#8)
    If Geocoder can't find your location, then view New York's as a 
    fallback. Also add a tooltip to the icons to help UX.

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,6 +1,8 @@
 class ForecastsController < ApplicationController
   def index
-    location = params[:q].present? ? params[:q] : "46615"
+    # If Geocoder can't find your location, then view New York's
+    browser_location = request.location.postal_code || "New York"
+    location = params[:q].present? ? params[:q] : browser_location
     @forecast_data = WeatherApiService.new(location).request_forecast_data
   end
 end
